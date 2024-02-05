6e76efdd9f53acc11b6089cffd1b72cbb2cbefe9
JuhyunSeo <67258446+JuhyunSeo@users.noreply.github.com>
2024-02-05 11:12:14 +0900

    Update ch05_chatbot_example.py

--- ch05_chatbot_example.py
+++ ch05_chatbot_example.py
@@ -9,6 +9,7 @@ from openai.embeddings_utils import get_embedding
 import streamlit as st
 from streamlit_chat import message
 
+openai.api_key = st.secrets['api_key']
 
 #folder_path와 folder_name을 결합하여 file_path = ./data/embedding.csv'
 folder_path = './data'
